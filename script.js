94739f283798c50a29620631aaedebc2fa663ac0
Erick Silva <donizttierick@gmail.com>
2025-12-22 10:12:27 -0300

    Ajusrtando

---
+++ script.js
@@ -0,0 +1,29 @@
+// Array com as imagens da forca
+let imagens = ["images/parte2.png", "images/parte3.png", "images/parte4.png", "images/parte5.png", "images/parte6.png", "images/parte7.png", "images/parte8.png"]
+
+// A frase correta é Java Script
+let palavra = ["J", "A", "V", "A", "S", "C", "R", "I", "P", "T"]
+
+// Saidas 
+let letraJ = document.getElementById("primeiro") // Letra J
+
+let letraA = document.querySelector(".repete") // Letra A
+
+let letraV = document.getElementById("segundo") // Letra V
+
+let letraS = document.getElementById("terceiro") // Letra S
+
+let letraC = document.getElementById("quarto") // Letra C
+
+let letraR = document.getElementById("quinto") // Letra R
+
+let letraI = document.getElementById("sexto")   // Letra I
+
+let letraP = document.getElementById("setimo")  // Letra P
+
+let letraT = document.getElementById("ultimo") //Letra T
+
+
+function letra(A) {
+
+}
