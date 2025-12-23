342482791ac840f1a3ffd549bbbba7679ae66d32
Erick Silva <donizttierick@gmail.com>
2025-12-23 10:04:56 -0300

    iNICIADO

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 // Array com as imagens da forca
 let imagens = ["images/parte2.png", "images/parte3.png", "images/parte4.png", "images/parte5.png", "images/parte6.png", "images/parte7.png", "images/parte8.png"]
 
-// A frase correta é Java Script
-let palavra = ["J", "A", "V", "A", "S", "C", "R", "I", "P", "T"]
-
 // Saidas 
 let letraJ = document.getElementById("primeiro") // Letra J
 
-let letraA = document.querySelector(".repete") // Letra A
+let letraAum = document.querySelector(".letraAum") // Letra A
 
 let letraV = document.getElementById("segundo") // Letra V
+
+let letraAdois = document.querySelector('.letraAdois') //LetraA
 
 let letraS = document.getElementById("terceiro") // Letra S
 
@@ -21,9 +20,52 @@ let letraI = document.getElementById("sexto")   // Letra I
 
 let letraP = document.getElementById("setimo")  // Letra P
 
+
 let letraT = document.getElementById("ultimo") //Letra T
 
+// Saida das letras que o usuario já digitou
+let r = document.getElementById('resultado')
 
-function letra(A) {
+//###########################################
+// Função de verificar as letras e ver se o usuario acertou
+//###########################################
+function VerificarLetra() {
+    // Input aode o usuario vai digitar 
+    let input = document.getElementById('letras').value.toUpperCase()
+
+    // Essa condição é para caso o usuario deixar o campo sem nada
+    if (input === '') {
+        alert('Preencha o Campo!')
+    }
+
+    // Essa condição é para caso o usuario digitar mais do que 1 letra
+    if (input.length > 1) {
+        alert('Escreva uma letra por vez!')
+    }
+
+    // Verificando se tem a letra J
+    if (input == 'J') {
+        letraJ.innerText = 'J'
+    }
+
+    if (input == 'A') {
+        letraAum.innerHTML = 'A'
+        letraAdois.innerHTML = 'A'
+    }
+    
+
+    if (input == 'V') {
+        letraV.innerText = 'V'
+    }
+
+
+
+
+
+    // POR UMA CONDIÇÃO SÓ PARA O ERRO, AI EU COLOCO O INDICE E A CADA ERRO ELE PERCORRE 1 ITEM DO ARRAY É COMO SE FOSSE UM BOTAO PASSANDO A IMAGENS PARA FRENTE
+
+    // Limpa os campos depois que o usuario clica no botão 
+    document.getElementById('letras').value = ''
 
 }
+
